d8fb2c91e535b9c5bc9d94252e7ead5749bdd055
Amar Hodžić <66332497+amarrh@users.noreply.github.com>
2021-09-05 18:06:22 +0200

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -135,7 +135,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 AWS_ACCESS_KEY_ID = 'AKIATS7AP5VSI3MP5FMO'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'amarh-portfolio'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.eu-central-1.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
