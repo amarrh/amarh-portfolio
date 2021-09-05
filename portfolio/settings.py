f7d2a614c1ea493b71682a237f92dc5874d6af1c
amarrh <amar_hodzic1@hotmail.com>
2021-09-05 10:44:27 +0200

    izmjenenove

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -139,6 +139,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
+AWS_S3_REGION_NAME = 'eu-central-1'
 AWS_LOCATION = 'static'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
